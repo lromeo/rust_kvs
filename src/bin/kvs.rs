26cbcc2e20be414874e1a9c91f7b825f9e857874
Luke Romeo <lromeo16@gmail.com>
2020-01-15 15:44:03 -0500

    Project 2 parts 1-4

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,8 +1,12 @@
 extern crate clap;
+extern crate failure;
+extern crate failure_derive;
+
 use clap::{App, Arg, SubCommand};
 use kvs::KvStore;
+use kvs::Result;
 
-fn main() {
+fn main() -> Result<()> {
     let matches = App::new("My Super Program")
         .version(env!("CARGO_PKG_VERSION"))
         .author(env!("CARGO_PKG_AUTHORS"))
@@ -15,24 +19,33 @@ fn main() {
         .subcommand(SubCommand::with_name("rm").arg(Arg::with_name("KEY").required(true).index(1)))
         .get_matches();
 
-    let mut store = KvStore::new();
+    let dir = std::env::current_dir().unwrap();
+    let mut store = KvStore::open(dir).unwrap();
 
     match matches.subcommand() {
         ("get", Some(sub_m)) => {
-            store.get(sub_m.value_of("KEY").unwrap().to_owned());
-            unimplemented!("unimplemented");
+            match store.get(sub_m.value_of("KEY").unwrap().to_owned())? {
+                Some(value) => println!("{}", value),
+                None => println!("Key not found"),
+            }
+
+            Ok(())
         }
         ("set", Some(sub_m)) => {
             store.set(
                 sub_m.value_of("KEY").unwrap().to_owned(),
                 sub_m.value_of("VALUE").unwrap().to_owned(),
-            );
-            unimplemented!("unimplemented");
+            )?;
+
+            Ok(())
         }
-        ("rm", Some(sub_m)) => {
-            store.remove(sub_m.value_of("KEY").unwrap().to_owned());
-            unimplemented!("unimplemented");
-        }
+        ("rm", Some(sub_m)) => match store.remove(sub_m.value_of("KEY").unwrap().to_owned()) {
+            Err(error) => {
+                println!("{}", error.as_fail());
+                Err(error)
+            }
+            Ok(_) => Ok(()),
+        },
         _ => panic!(),
-    };
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![deny(missing_docs)]
+// #![deny(missing_docs)]
 
 //! # PNA Rust Project - Key Value KvStore
 //!
@@ -14,35 +14,104 @@
 //! assert_eq!(store.get("key1".to_owned()), None);
 //! ```
 //!
+#[macro_use]
+extern crate failure_derive;
 
+use failure::Error;
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
+use std::fs::File;
+use std::fs::OpenOptions;
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::io::BufWriter;
+use std::path::PathBuf;
+
+// Some error type
+pub type Result<T> = std::result::Result<T, Error>;
 
 /// Object to set, get, and remove key value pairs
-#[derive(Default)]
 pub struct KvStore {
     data: HashMap<String, String>,
+    writer: BufWriter<File>,
+}
+
+#[derive(Fail, Debug)]
+#[fail(display = "Key not found")]
+pub struct KeyNotFound;
+
+#[derive(Serialize, Deserialize, Debug)]
+enum Command {
+    Set { key: String, value: String },
+    Remove { key: String },
 }
 
 impl KvStore {
-    /// Construct a new KvStore with no stored key value pairs
-    pub fn new() -> KvStore {
-        KvStore {
-            data: HashMap::new(),
-        }
-    }
-
     /// Set a key value pair to be accessible later
-    pub fn set(&mut self, key: String, value: String) {
+    pub fn set(&mut self, key: String, value: String) -> Result<()> {
+        let c = Command::Set {
+            key: key.clone(),
+            value: value.clone(),
+        };
+
+        self.log(c);
+
         self.data.insert(key, value);
+
+        Ok(())
     }
 
     /// Get a previously set value for a key
-    pub fn get(&self, key: String) -> Option<String> {
-        self.data.get(&key).cloned()
+    pub fn get(&self, key: String) -> Result<Option<String>> {
+        Ok(self.data.get(&key).cloned())
     }
 
     /// Remove a previously set key value
-    pub fn remove(&mut self, key: String) {
-        self.data.remove(&key);
+    pub fn remove(&mut self, key: String) -> Result<()> {
+        let c = Command::Remove { key: key.clone() };
+
+        self.log(c);
+
+        match self.data.remove(&key) {
+            None => Err(Error::from(KeyNotFound)),
+            Some(_value) => Ok(()),
+        }
+    }
+
+    fn log(&mut self, command: Command) {
+        let mut s = serde_json::to_string(&command).unwrap();
+
+        s.push_str("\n");
+
+        self.writer.write(&s.into_bytes()).unwrap();
+    }
+
+    pub fn open(path: impl Into<PathBuf>) -> Result<KvStore> {
+        let mut path = path.into();
+        path.push("log");
+
+        let f = OpenOptions::new()
+            .read(true)
+            .append(true)
+            .create(true)
+            .open(&path)
+            .unwrap();
+
+        // Load previous data from log
+        let mut data = HashMap::new();
+
+        for line in BufReader::new(&f).lines() {
+            let command: Command = serde_json::from_str(&line.unwrap())?;
+
+            match command {
+                Command::Set { key, value } => data.insert(key, value),
+                Command::Remove { key } => data.remove(&key),
+            };
+        }
+
+        Ok(KvStore {
+            data: data,
+            writer: BufWriter::new(f),
+        })
     }
 }
